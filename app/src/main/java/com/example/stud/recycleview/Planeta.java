07523f0915c690c50dbbfbdf1776d577b334bbd2
Magdalena Kichman <w57015@student.wsiz.rzeszow.pl>
2018-04-12 18:35:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stud/recycleview/Planeta.java
@@ -0,0 +1,19 @@
+package com.example.stud.recycleview;
+
+/**
+ * Created by W57015 on 2018-04-12.
+ */
+
+public class Planeta {
+
+     String nazwa;
+     String rodzaj;
+     int ksiezyce;
+
+     public Planeta(String nazwa, String rodzaj, int ksiezyce ) {
+
+         this.nazwa = nazwa;
+         this.rodzaj = rodzaj;
+         this.ksiezyce = ksiezyce;
+     }
+}
